d206a24d9a6364a57d87854d46d2d2ebaa09f067
Tomas Dominguez <dominguez.tom@icloud.com>
2017-02-27 19:42:20 -0500

    Creadted new flashy program

---
+++ falshy/falshy/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace falshy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            while (Visible)
+            {
+                for (int i = 0; i < 253 && Visible; i++)
+                {
+                    for (int j = 254; j > 0; j--)
+                    {
+                        int c = j;
+
+                        this.BackColor = Color.FromArgb(c, 255 - c, c);
+
+                        Application.DoEvents();
+
+                        System.Threading.Thread.Sleep(3);
+
+                        i -= j;
+                    }
+                }
+
+                //for (int c = 0; c < 254 && Visible; c++)
+                //{
+                //    this.BackColor = Color.FromArgb(c, 255 - c, c);
+
+                //    Application.DoEvents();
+
+                //    System.Threading.Thread.Sleep(3);
+                //}
+
+                //for (int c = 0; c > 0 && Visible; c--)
+                //{
+                //    this.BackColor = Color.FromArgb(c, 255 - c, c);
+
+                //    Application.DoEvents();
+
+                //    System.Threading.Thread.Sleep(3);
+                //}
+            }
+        }
+    }
+}
+
